d46d3731d0e5f7f730b370eda32ea9776c6b0aea
Gang Liang <randomizedthinking@gmail.com>
2022-12-06 11:56:00 -0800

    Add config setting for todo-comments

---
+++ after/plugin/02-plug-todo-comments.lua
@@ -0,0 +1,14 @@
+require("todo-comments").setup{
+  keywords = {
+    FIX = {
+      icon = " ",
+      color = "error",
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+    },
+    TODO = { icon = " ", color = "info" },
+    HACK = { icon = " ", color = "warning" },
+    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } }
+  },
+  merge_keywords = false,
+}
